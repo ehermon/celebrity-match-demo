7690e4f7b9e20613d7281764fed7255456cfa102
ehermon <ehermon@softlayer.com>
2016-11-03 15:23:22 +0100

    Update config.js
    added personality insight credentials 
    added twitter consumer key, consumer secret key, access token key and access token secret

--- config/config.js
+++ config/config.js
@@ -22,17 +22,17 @@ module.exports = {
 
   // Personality Insights credentials
   personality_insights: {
-    url: '<url>',
-    username: '<username>',
-    password: '<password>',
+    url: 'https://gateway.watsonplatform.net/personality-insights/api',
+    username: 'imIC7IW8HjkJ',
+    password: '4eecf4cb-4038-47e4-bf38-1f1a64196923',
     version: 'v2'
   },
 
   // Twitter app credentials: https://apps.twitter.com/app
   twitter: process.env.TWITTER ? JSON.parse(process.env.TWITTER) : [{
-    consumer_key: '<consumer_key>',
-    consumer_secret: '<consumer_secret>',
-    access_token_key: '<access_token_key>',
-    access_token_secret: '<access_token_secret>'
+    consumer_key: 'qPwl6ZnSkkKE3kNMxYeDWsrbD',
+    consumer_secret: 'McwpBHQDTEAtgiQ1da0HdOT1m1vTxzILTLg1hhKz0ipB2oqnte',
+    access_token_key: '4071428416-89A2tUWP8hOA84gBYP0NXOD5yaDHP8wD1iXcGcs',
+    access_token_secret: '18HZ7kidR6HEcxxCkcOmAIqedT0dchNFcEI2g8F6tL2Tu'
   }]
 };
